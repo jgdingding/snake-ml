ff7455b56df06e2b9d23da558cb79217e7287e6d
williamyao66 <williamyao2001@gmail.com>
2020-06-29 22:25:15 -0400

    first prototype

--- gameDisplay.py
+++ gameDisplay.py
@@ -1,8 +1,83 @@
-import tkinter as tk
+from tkinter import *
 from snakeGame import *
+from time import *
 
-root = tk.Tk()
-frame = Frame(root)
+snakee = []
+foood = []
+def drawBoard(root, s, w):
+    for i in range(len(s.snake)):
+        if i > 0:
+            snakee.append(w.create_oval(s.snake[i][0]*20+44,s.snake[i][1]*20+44,s.snake[i][0]*20+56, s.snake[i][1]*20+56, fill = "black"))
+        else:
+            snakee.append(w.create_oval(s.snake[i][0]*20+44,s.snake[i][1]*20+44,s.snake[i][0]*20+56, s.snake[i][1]*20+56, fill = "blue"))
 
-root.mainloop()
-root.destroy()
+    foood.append(w.create_oval(s.food[0]*20+44, s.food[1]*20+44, s.food[0]*20+56, s.food[1]*20+56, fill = "red"))
+    w.pack()
+
+    return
+
+def updateSnake(root, s, w, direct):
+    sleep(0.5)
+    for i in direction:
+        if direct == i.name:
+            val = s.moveSnake(i.value)
+            if val == -1:
+                snakee.insert(0, w.itemconfig(foood.pop(), fill = "blue"))
+                w.itemconfig(snakee[1], fill = "black")
+                foood.append(w.create_oval(s.food[0]*20+44, s.food[1]*20+44, s.food[0]*20+56, s.food[1]*20+56, fill = "red"))
+
+            elif val == 1:
+                moveme = snakee.pop()
+                w.itemconfig(moveme, fill = "blue")
+                x = ((s.snake[0][0] + i.value[0]) * 20) - (s.snake[-1][0] * 20)
+                y = ((s.snake[0][1] + i.value[1]) * 20) - (s.snake[-1][1] * 20)
+                w.move(moveme, x, y)
+                snakee.insert(0, moveme)
+                w.itemconfig(snakee[1], fill = "black")
+    root.update()
+    return
+
+def main():
+    root = Tk()
+    root.geometry('500x500')
+    
+    w = Canvas(root, width = 500, height = 500)
+
+    line = w.create_line(50,50,50,450)
+    line = w.create_line(450,450,450,50)
+    line = w.create_line(50,50,450,50)
+    line = w.create_line(50,450,450,450)
+
+    for i in range(70, 450, 20):
+        line = w.create_line(i,50,i,450)
+
+    for j in range(70, 450, 20):
+        line = w.create_line(50,j,450,j)
+
+    s = SnakeGame()
+
+    drawBoard(root, s, w)
+    root.update()
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    direct = "right"
+    updateSnake(root, s, w, direct)    
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    direct = "right"
+    updateSnake(root, s, w, direct)    
+    direct = "right"
+    updateSnake(root, s, w, direct)    
+    direct = "right"
+    updateSnake(root, s, w, direct)    
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    direct = "right"
+    updateSnake(root, s, w, direct)
+    root.mainloop()
+
+main()
--- snakeGame.py
+++ snakeGame.py
@@ -24,7 +24,7 @@ class items(Enum):
 class SnakeGame:
 
     boardSize = 20
-    snake = [(3, 10), (4, 10), (5, 10)]  # 0th element represents head
+    snake = [(5, 10), (4, 10), (3, 10)]  # 0th element represents head
     snakeLength = 3
     facing = direction.right
 
@@ -34,22 +34,25 @@ class SnakeGame:
         self.board = [[items.blank.value]*20]*20
 
     def moveSnake(self, dtn):
-        newCoord = (self.snake[0][0] + dtn.value[0],
-                    self.snake[0][1] + dtn.value[1])
+        newCoord = (self.snake[0][0] + dtn[0],
+                    self.snake[0][1] + dtn[1])
         if newCoord[0] == self.food[0] and newCoord[1] == self.food[1]:  # if we hit the food
             self.snake.insert(0, newCoord)
             self.snakeLength += 1
             self.generateFood()
             self.board[newCoord[0]][newCoord[1]] = items.snake.value
+            return -1
         # check if snake goes out of bounds or hits own tail
         elif newCoord in self.snake or newCoord[0] > 19 or newCoord[0] < 0 or newCoord[1] > 19 or newCoord[1] < 0:
             self.snakeLength = 0
+            return 0
         else:
             self.snake.insert(0, newCoord)
             oldTail = self.snake.pop()
             self.board[oldTail[0]][oldTail[1]] = items.blank.value
             self.board[newCoord[0]][newCoord[1]] = items.snake.value
-        return
+            return 1
+        return 0
 
     def generateFood(self):
         self.board[self.food[0]][self.food[1]] = items.blank.value
